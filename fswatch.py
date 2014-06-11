af326f9ea76108b3957c85f14b900b910aee694b
zjx20 <zhoujianxiong2@gmail.com>
2014-06-11 18:06:59 +0800

    removed gui and supported syncing deleted file

--- fswatch.py
+++ fswatch.py
@@ -14,15 +14,49 @@ Note: if you are running against a large directory, it will
 take awhile at the beginning, my hunch is it needs to traverse
 all sub-directories and attach the listeners everywhere
 
+tested on OS X 10.9.3
 """
+
+"""
+Reference of FSEventStreamEventFlags,
+https://developer.apple.com/library/mac/documentation/Darwin/Reference/FSEvents_Ref/Reference/reference.html
+
+enum {
+   kFSEventStreamEventFlagNone = 0x00000000,
+   kFSEventStreamEventFlagMustScanSubDirs = 0x00000001,
+   kFSEventStreamEventFlagUserDropped = 0x00000002,
+   kFSEventStreamEventFlagKernelDropped = 0x00000004,
+   kFSEventStreamEventFlagEventIdsWrapped = 0x00000008,
+   kFSEventStreamEventFlagHistoryDone = 0x00000010,
+   kFSEventStreamEventFlagRootChanged = 0x00000020,
+   kFSEventStreamEventFlagMount = 0x00000040,
+   kFSEventStreamEventFlagUnmount = 0x00000080, /* These flags are only set if you specified the FileEvents*/
+   /* flags when creating the stream.*/
+   kFSEventStreamEventFlagItemCreated = 0x00000100,
+   kFSEventStreamEventFlagItemRemoved = 0x00000200,
+   kFSEventStreamEventFlagItemInodeMetaMod = 0x00000400,
+   kFSEventStreamEventFlagItemRenamed = 0x00000800,
+   kFSEventStreamEventFlagItemModified = 0x00001000,
+   kFSEventStreamEventFlagItemFinderInfoMod = 0x00002000,
+   kFSEventStreamEventFlagItemChangeOwner = 0x00004000,
+   kFSEventStreamEventFlagItemXattrMod = 0x00008000,
+   kFSEventStreamEventFlagItemIsFile = 0x00010000,
+   kFSEventStreamEventFlagItemIsDir = 0x00020000,
+   kFSEventStreamEventFlagItemIsSymlink = 0x00040000
+};
+"""
+
 import os, datetime, time       # python packages
 import fsevents                 # https://pypi.python.org/pypi/MacFSEvents
 import Tkinter
+import sys
 
 # CONFIG PARAMS, set envirovnment variables or hardcode
 # include trailing slashes for rsync, being more explicit is better
 config = {
     'remote_host': 'user@remote.server.com',
+    # try this if you are meeting non-standard ssh port
+    # 'remote_host': "--rsh='ssh -p2222' user@remote.server.com",
     'watch': [
         {
             'local': '/your/local/path1/',
@@ -39,10 +73,9 @@ config = {
 ignore_list = ['.svn', '.DS_Store', '.git']
 
 def display(str):
-    global ta
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     mystr = "[{0}] {1} \n".format(now, str)
-    ta.insert(Tkinter.END, mystr)
+    print mystr
 
 
 def file_event_sync(event):
@@ -61,35 +94,64 @@ def file_event_sync(event):
     for ig in ignore_list:                          # check ignore list
         if ig in filename:
             return
-    
-    # basic rsync to remote server
-    cmd = " rsync -cazq --del %s %s:%s%s " % (filename, config['remote_host'], remote_path, remote_file)
-    display("Syncing %s " % filename)
+
+    kFSEventStreamEventFlagUserDropped = 0x00000002
+    kFSEventStreamEventFlagMount = 0x00000040
+    kFSEventStreamEventFlagItemCreated = 0x00000100
+    kFSEventStreamEventFlagItemRemoved = 0x00000200
+
+    if (event.mask & kFSEventStreamEventFlagItemCreated) != 0:
+        # created
+        remote_parent = os.path.dirname(remote_file)
+        cmd = " rsync -cazq --delete %s %s:%s%s/ " % (filename, config['remote_host'], remote_path, remote_parent)
+    elif (event.mask & kFSEventStreamEventFlagItemRemoved) != 0 or \
+            (event.mask & kFSEventStreamEventFlagMount) != 0:
+        # removed or renamed
+        # no idea why kFSEventStreamEventFlagMount is for renaming
+        local_parent = os.path.dirname(filename)
+        remote_parent = os.path.dirname(remote_file)
+        cmd = " rsync -cazq --delete %s/ %s:%s%s/ " % (local_parent, config['remote_host'], remote_path, remote_parent)
+    elif (event.mask & kFSEventStreamEventFlagUserDropped) != 0:
+        # modified
+        # no idea why kFSEventStreamEventFlagUserDropped is for modifying
+        cmd = " rsync -cazq --delete %s %s:%s%s " % (filename, config['remote_host'], remote_path, remote_file)
+    else:
+        display("[WARNING] Unsupported event: %s" % str(event))
+        return
+
+    display("Syncing %s" % filename)
     os.system(cmd)
 
 
 ## Main
 
-## Setup GUI
-canvas = Tkinter.Tk()
-scroll = Tkinter.Scrollbar(canvas)
-ta = Tkinter.Text(canvas)
-
-scroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
-ta.pack(side=Tkinter.LEFT, fill=Tkinter.Y)
-scroll.config(command=ta.yview)
-ta.config(yscrollcommand=scroll.set)
+if len(sys.argv) > 1 and sys.argv[1] == "--full":
+    full_sync_matcher = None
+    if len(sys.argv) > 2:
+        full_sync_matcher = sys.argv[2]
+    for watch in config['watch']:
+        if full_sync_matcher != None and watch['local'].find(full_sync_matcher) == -1:
+            continue
+        print "Full sync from '%s' to %s:%s" % (watch['local'], config['remote_host'], watch['remote'])
+        cmd = " rsync -cazqr --delete %s %s:%s " % (watch['local'], config['remote_host'], watch['remote'])
+        print cmd
+        os.system(cmd)
+    exit(0)
 
 ## Setup Watcher
 observer = fsevents.Observer()
-observer.start()
 for watch in config['watch']:
     stream = fsevents.Stream(file_event_sync, watch['local'], file_events=True)
     observer.schedule(stream)
     display("Watching: %s " % watch['local'])
 
-## running
-canvas.mainloop()
+observer.start()
+
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
 
 ## clean-up
 observer.unschedule(stream)
